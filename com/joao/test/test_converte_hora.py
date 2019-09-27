e4185c76ffe6f3ee52c96ae7843aba50d10ce075
JoÒo Victor da Silva Matos <1900977@SALAS.AULAS>
2019-09-26 21:20:24 -0300

    arrumando teste converte_hora

--- com/joao/test/test_converte_hora.py
+++ com/joao/test/test_converte_hora.py
@@ -1,14 +1,29 @@
 import pytest
 from com.joao.converte_hora import converteHora
 
-def test_converte_hora():
-	assert converteHora(-3, 48) == None
-	
-def test_converte_manha():
-	assert converteHora(8, 10) == "08:10 AM"
-	
-def test_converte_tarde():
-	assert converteHora(21, 20) == "09:20 PM"
-	
-def test_converte_hora():
-	assert converteHora(10, 63) == None
+def test_hora_hr_neg():
+    assert converteHora(-12, 0) == None
+
+
+def test_hora_min_neg():
+    assert converteHora(0, -8) == None
+
+
+def test_hr_min_post_extra():
+    assert converteHora(0, 70) == None
+
+
+def test_hr_post_extra():
+    assert converteHora(24, 0) == None
+
+
+def test_hr_menor_doze():
+    assert converteHora(10, 0) == '10:00 AM'
+    
+
+def test_hr_zerada():
+    assert converteHora(0, 0) == '12:00 AM'
+    
+
+def test_hr_maior_doze():
+    assert converteHora(15, 0) == '03:00 PM'
